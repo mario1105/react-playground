0dcb168f76d0a6f856ec0a35dd90c2a1def5f35b
mario.jimenez <mario.jimenez@theshopworks.com>
2024-08-13 01:59:12 +0100

    Display genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -5,7 +5,7 @@ export interface Genre {
   name: string;
   //   slug: string;
   //   games_count: number;
-  //   image_background: string;
+  image_background: string;
 }
 
 const useGenres = () => useData<Genre>("/genres");
